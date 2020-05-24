f3c627912d0c8dff25f018c2f476d3d58fca2396
gabriel paiva <gab@MacBook-Pro-de-gabriel.local>
2020-05-24 19:04:54 -0300

    Added list categories routes

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import { getRepository } from 'typeorm';
+
+import Category from '../models/Category';
+
+const categoriesRouter = Router();
+
+categoriesRouter.get('/', async (request, response) => {
+  const categoriesRepository = getRepository(Category);
+
+  const categories = await categoriesRepository.find({});
+
+  return response.json(categories);
+});
+
+export default categoriesRouter;
